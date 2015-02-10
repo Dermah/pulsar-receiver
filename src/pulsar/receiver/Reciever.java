6dda8846c2d7d1f7b00ad3790c11e53a4c60440f
Sputnik Antolovich <inquiry@dermah.com>
2015-02-11 01:07:06 +1100

    Add basic receiver package files

---
+++ src/pulsar/receiver/Reciever.java
@@ -0,0 +1,10 @@
+package pulsar.receiver;
+
+public class Reciever {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("What the hell is this");
+	}
+
+}
