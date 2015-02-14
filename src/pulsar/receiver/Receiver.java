03bddcc8181bdf51abeb2b347a69c6133ae2ca43
Sputnik Antolovich <inquiry@dermah.com>
2015-02-15 00:27:50 +1100

    Reformat whitespace

--- src/pulsar/receiver/Receiver.java
+++ src/pulsar/receiver/Receiver.java
@@ -4,20 +4,20 @@ import processing.core.*;
 
 public class Receiver extends PApplet {
 
-	public void setup () {
-		size(displayWidth, displayHeight);
-		background(0);
-	}
-	
-	public void draw () {
-		
-	}
+  public void setup () {
+    size(displayWidth, displayHeight);
+    background(0);
+  }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("What the hell is this");
-		
-		PApplet.main(new String[] { "--present", "pulsar.receiver.Receiver" });
-	}
-	
+  public void draw () {
+
+  }
+
+  public static void main (String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("What the hell is this");
+
+    PApplet.main(new String[] { "--present", "pulsar.receiver.Receiver" });
+  }
+
 }
