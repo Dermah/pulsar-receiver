b64b647d2a6e188e90fd2ceab128f498e2ede070
Dermah <inquiry@dermah.com>
2015-03-19 20:12:23 +1100

    Make the receiver open a UDP port to listen for pulses

--- src/pulsar/receiver/Receiver.java
+++ src/pulsar/receiver/Receiver.java
@@ -4,17 +4,21 @@ import processing.core.*;
 
 public class Receiver extends PApplet {
 
+  private UDPListener udp;
+  
   public void setup () {
+    udp = new UDPListener();
+    
     size(displayWidth, displayHeight);
     background(0);
   }
 
   public void draw () {
-
+	  
   }
 
   public static void main (String[] args) {
-    // TODO Auto-generated method stub
+    
     System.out.println("What the hell is this");
 
     PApplet.main(new String[] { "--present", "pulsar.receiver.Receiver" });
